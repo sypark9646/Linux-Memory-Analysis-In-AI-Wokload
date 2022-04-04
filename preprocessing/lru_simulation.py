316d032b42f552c52b3b683798ea56d24b156343
soyeon Park <sypark9646@gmail.com>
2022-04-04 14:15:46 +0900

    check whether key in cache by calculated rank

--- preprocessing/lru_simulation.py
+++ preprocessing/lru_simulation.py
@@ -8,11 +8,12 @@ Original file is located at
 """
 
 from collections import OrderedDict
+from alive_progress import alive_bar
 import argparse
 import os
 import sys
-from alive_progress import alive_bar
 import time
+import json
 
 class LRUCache: # Both reads & writes affect rank changes
     def __init__(self):
@@ -27,18 +28,23 @@ class LRUCache: # Both reads & writes affect rank changes
             rank = index
             break
           index += 1
-        if key in self.cache:
+        if rank != -1: # if key in the cache list
           self.cache.move_to_end(key, last=False) # move the key to the front when recently used
         return rank
  
     def write(self, key: int) -> int:
         rank = self.read(key)
-        if key not in self.cache:
-          self.cache[key] = 1
+        if rank == -1: # if key is not in the cache list
+          self.cache[key] = None
           self.cache.move_to_end(key, last=False)
         return rank
 
-def tocsv(pointer, write_file_name, ranking_access):
+def tojson(pointer, write_file_name, ranking_access):
+  with open(f"{write_file_name}_LRU_{pointer}.json", 'w') as fp:
+    json.dump(my_dict, fp)
+  print("saved", f"{write_file_name}_LRU_{pointer}.json")
+
+def totxt(pointer, write_file_name, ranking_access):
   file_write = open(f"{write_file_name}_LRU_{pointer}.txt", "w");
   file_write.write("rank access_number\n")
   for rank in sorted(ranking_access):
@@ -49,7 +55,7 @@ def tocsv(pointer, write_file_name, ranking_access):
 def main(read_file_name, STEP):
   print("process", read_file_name, "and save every", str(STEP), "lines.")
   index = read_file_name.rfind(".")
-  write_file_name = read_file_name[:index]
+  write_file_name = read_file_name[:-4]
 
   ranking_access = {}
   cache = LRUCache()
@@ -63,7 +69,7 @@ def main(read_file_name, STEP):
         rank = -1
         if data_type.startswith('read'):
           rank = cache.read(block_address)
-        elif data_type.startswith('write'):
+        else: #data_type.startswith('write'):
           rank = cache.write(block_address)
 
         if rank != -1:
@@ -72,9 +78,10 @@ def main(read_file_name, STEP):
     
         pointer += 1
         time.sleep(.005)
-        if pointer == num_lines or pointer % STEP == 0:
-          tocsv(pointer, write_file_name, ranking_access) # check point
+        if pointer % STEP == 0:
+          tojson(pointer, write_file_name, ranking_access) # check point
         bar()
+  totxt(pointer, write_file_name, ranking_access)
 
 def is_valid_file(parser, arg):
   if not os.path.exists(arg):
