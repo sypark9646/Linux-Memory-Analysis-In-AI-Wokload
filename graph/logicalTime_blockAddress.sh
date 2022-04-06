008ff44fdd1256923fde03a7086eeec73a452fca
soyeon Park <sypark9646@gmail.com>
2022-04-06 09:39:10 +0900

    Update logicalTime_blockAddress.sh

--- graph/logicalTime_blockAddress.sh
+++ graph/logicalTime_blockAddress.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 gnuplot -p -e "set datafile separator ','; \
-	set style fill solid border \
+	set style fill solid border; \
 	set style circle radius 0.01; \
-	set output '/home/soyeon/logicalTime_blockAddress.png' \
-	plot '/home/soyeon/memdf.csv' using 0:4"
+	set output '/home/soyeon/logicalTime_blockAddress.png'; \
+	plot '/home/soyeon/memdf.csv' using 0:4;"
