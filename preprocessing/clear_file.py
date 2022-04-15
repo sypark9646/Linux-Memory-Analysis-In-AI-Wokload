5d7871f1932b59690148029956023b69df9a99e1
soyeon Park <sypark9646@gmail.com>
2022-04-15 16:28:45 +0900

    Update clear_file.py

--- preprocessing/clear_file.py
+++ preprocessing/clear_file.py
@@ -11,10 +11,9 @@ import argparse
 import os
 import sys
 
-def main(read_file_name):
+def main(read_file_name, write_file_name, start_time, end_time):
   print("clear", read_file_name)
-  index = read_file_name.rfind(".")
-  write_file_name = read_file_name[:index]+"_clear.txt"
+  write_file_name = write_file_name+"_clear.txt"
   file_write = open(write_file_name, "w");
   file_write.write("type address size block_address\n") # write header
   file_write.close()
@@ -24,10 +23,13 @@ def main(read_file_name):
       for line in f:
         if (line.startswith('read') or line.startswith('write')):
           data_line = line.split()
-          data_line.pop()
-          block_address = int(str(data_line[1]), 16)
-          data_line.append(str(block_address))
-          file_write.write(" ".join(data_line)+"\n")
+          if float(data_line[-1]) > end_time:
+            break
+          if float(data_line[-1]) <= start_time:
+            data_line.pop()
+            block_address = int(str(data_line[1]), 16)
+            data_line.append(str(block_address))
+            file_write.write(" ".join(data_line)+"\n")
   file_write.close()
 
 def is_valid_file(parser, arg):
@@ -39,8 +41,13 @@ def is_valid_file(parser, arg):
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description = 'remove timestamp and calculate block address')
-  parser.add_argument("-i", dest="filename", required=True,
-                    help="input raw trace data",
+  parser.add_argument("-input", dest="input", required=True,
                     type=lambda x: is_valid_file(parser, x))
+  parser.add_argument("-output", dest="output", required=True,
+                    type=str)
+  parser.add_argument("-start", dest="start", required=True,
+                    type=float)
+  parser.add_argument("-end", dest="end", required=True,
+                    type=float)
   args = parser.parse_args()
-  main(args.filename)
+  main(args.input, args.output, args.start, args.end)
